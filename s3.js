cb702b441fa5c474d3d0f1e1601ca3c70adb0691
CoxeterLabs <89303732+CoxeterLabs@users.noreply.github.com>
2026-06-30 10:27:04 +0300

    Change log message from 'Winner'

---
+++ s3.js
@@ -0,0 +1,8 @@
+(() => {
+  console.log("[Winner Test] Script loaded successfully", {
+    url: window.location.href,
+    time: new Date().toISOString()
+  });
+
+  alert("DMBObet test script loaded successfully!");
+})();
